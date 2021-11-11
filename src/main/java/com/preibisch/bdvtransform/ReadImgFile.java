ca47d627a5911f258d5ad23945fe9055ac61c98b
mzouink <zouinkhi.marwan@gmail.com>
2021-11-11 11:12:26 -0500

    load nrrd into bdv

--- src/main/java/com/preibisch/bdvtransform/ReadImgFile.java
+++ src/main/java/com/preibisch/bdvtransform/ReadImgFile.java
@@ -1,5 +1,6 @@
 package com.preibisch.bdvtransform;
 
+import bdv.util.BdvFunctions;
 import ij.ImagePlus;
 import io.IOHelper;
 import net.imglib2.img.ImagePlusAdapter;
@@ -11,19 +12,21 @@ import java.io.File;
 import java.io.IOException;
 
 public class ReadImgFile {
-    private static final String TEST_IMAGE_PATH = "/Users/Marwan/Desktop/cellmap/Task/data/PALM_532nm_gauss3d_d2.nrrd";
+    private static final String TEST_IMAGE1_PATH = "/Users/Marwan/Desktop/cellmap/Task/data/PALM_532nm_gauss3d_d2.nrrd";
 
     public static < T extends NumericType< T > & NativeType< T >> void main(String[] args) throws IOException {
-        File file = new File( TEST_IMAGE_PATH );
+        File file = new File( TEST_IMAGE1_PATH );
         if (!file.exists())
             throw new IOException("File not found! Wrong path: "+file.getAbsolutePath());
 
         System.out.println("Opening: "+file.getAbsolutePath());
         ImagePlus imp = new IOHelper().readIp(file);
 //        ImagePlus imp = IJ.openImage(file.getAbsolutePath());
-        imp.show();
+//        imp.show();
+
 
         // wrap it into an ImgLib image (no copying)
         final Img< T > image = ImagePlusAdapter.wrap( imp );
+        BdvFunctions.show(image,"Input");
     }
 }
